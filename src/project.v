b7d10743c0ffc0c6fdb33729a24f6078f75c4ab8
Trịnh Minh Quang <109939424+itsQuang@users.noreply.github.com>
2025-07-02 15:19:48 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -25,7 +25,7 @@ module tt_um_asiclab_example (
     wire _unused = &{ena, uio_in, 1'b0};
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            uo_out <= 0
+            uo_out <= 0;
         end else begin
             uo_out[3:0] <= ui_in[7:4] + ui_in[3:0];
             uo_out[7:4] <= 0;
